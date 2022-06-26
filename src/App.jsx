e98b604b9fc9200484cfb5d70c780214b0266246
musleh0001 <khan.muslehuddin0001@gmail.com>
2022-06-26 21:11:15 +0600

    feat: implement leaflet map

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import ForgotPassword from "./pages/ForgotPassword";
 import PrivateRoute from "./components/PrivateRoute";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 
 function App() {
 	return (
@@ -28,6 +30,8 @@ function App() {
 					<Route path="/sign-up" element={<SignUp />} />
 					<Route path="/forgot-password" element={<ForgotPassword />} />
 					<Route path="/create-listing" element={<CreateListing />} />
+					<Route path="/category/:categoryName/:listingId" element={<Listing />} />
+					<Route path="/contact/:landlordId" element={<Contact />} />
 				</Routes>
 				<Navbar />
 			</Router>
